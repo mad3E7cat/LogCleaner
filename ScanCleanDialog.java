3e07f4aa9ec85ab09e93c7e9acb2403d9e6559c2
mad3e7cat <workmail543@mail.ru>
2017-07-12 03:44:16 +0300

    serach files init

--- ScanCleanDialog.java
+++ ScanCleanDialog.java
@@ -8,9 +8,10 @@ import java.awt.event.ItemEvent;
 import java.awt.event.KeyEvent;
 class ScanCleanDialog extends JDialog{
 	//
-	public ScanCleanDialog(JFrame owner){
+	public ScanCleanDialog(DialogFrame owner){ // JFrame
 		super(owner, "Scan and Clean", true);
-		//
+		setSize(300, 200);
+		//		
 		JProgressBar progressBar = new JProgressBar();
 		JLabel progressLabel = new JLabel("Scanning for logs...", javax.swing.SwingConstants.CENTER);
 		progressBar.setSize(300, 25);
@@ -18,7 +19,15 @@ class ScanCleanDialog extends JDialog{
 		progressLabel.add(progressBar);
 		add(progressLabel, BorderLayout.CENTER);
 		//
-		
-		setSize(300, 200);	
+		if(owner.isWindows()){
+			
+			//
+			setSize(600, 400);
+		}
+		if(owner.isDebian()){
+			
+			//
+			setSize(600, 400);
+		}
 	}
 }
